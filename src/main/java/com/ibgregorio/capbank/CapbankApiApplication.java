d25cbb234d45a87cb9bce3daca15b1e46aa1d498
Ítalo Gregório <italo-07@hotmail.com>
2020-06-17 18:03:16 -0300

    Commit inicial

---
+++ src/main/java/com/ibgregorio/capbank/CapbankApiApplication.java
@@ -0,0 +1,13 @@
+package com.ibgregorio.capbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapbankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapbankApiApplication.class, args);
+	}
+
+}
